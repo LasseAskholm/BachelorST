1f428c310c756251e41b699e4a4eef3d0faa32e5
LasseAskholm <71758394+LasseAskholm@users.noreply.github.com>
2023-09-05 14:06:28 +0200

    Init

---
+++ alpacaLoraTest.py
@@ -0,0 +1,49 @@
+import numpy as np
+import textwrap
+from transformers import LlamaTokenizer, LlamaForCausalLM
+import os
+import sys
+from typing import List
+
+from peft import (
+    LoraConfig,
+    get_peft_model,
+    get_peft_model_state_dict,
+    prepare_model_for_int8_training,
+)
+
+import fire
+import torch
+from datasets import load_dataset
+import pandas as pd
+ 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+from pylab import rcParams
+
+print (torch.cuda.is_available())
+
+sns.set(rc={'figure.figsize':(10, 7)})
+sns.set(rc={'figure.dpi':100})
+sns.set(style='white', palette='muted', font_scale=1.2)
+ 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+
+BASE_MODEL = "decapoda-research/llama-7b-hf"
+
+model = LlamaForCausalLM.from_pretrained(
+    BASE_MODEL,
+    load_in_8bit=True,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+ 
+tokenizer = LlamaTokenizer.from_pretrained(BASE_MODEL)
+ 
+tokenizer.pad_token_id = (
+    0  # unk. we want this to be different from the eos token
+)
+tokenizer.padding_side = "left"
