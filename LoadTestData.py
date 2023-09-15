8668dd68fa133475db86d18f13ccfaffcaae4b77
atj_laptop <akselthyregod@live.dk>
2023-09-15 13:37:37 +0200

    Added pick first. I.e. skip to end of entity token

--- LoadTestData.py
+++ LoadTestData.py
@@ -76,9 +76,71 @@ def map_entities(dict,path):
     return df
 
 
+def map_entities_pickFirstMatch(dict,path):
+    tags = []
+    words = []
+    document_ids = []
+    o_tags_idx = []
+
+    with open (path, 'r', encoding = "utf-8") as json_file:
+        for line in json_file:
+            document = json.loads(line)
+            entities = dict[document["_id"]]
+            text = document['text']
+            masked_text = document['text']
+            for k,v in entities.items():
+                entity = text[v["begin"]:v["end"]]
+                masked_text = masked_text[:v["begin"]] + "".join(["*" for _ in range(len(entity))]) + masked_text[v["end"]:]
+            begin_idx = 0
+            for word in masked_text.split(" "):
+                use_word = True
+                for char in word:
+                    if char == '*':
+                        use_word = False
+                        break
+                if use_word:
+                    o_tags_idx.append((begin_idx,begin_idx+len(word)+1))    
+                begin_idx += len(word)+1
+
+            current_o_tags_idx = 0
+            i = 0
+            while (i< len(text)):
+                entity_found = False
+                for k,v in entities.items():
+                    if i == v["begin"]:
+                        entity_found = True
+                        entity = text[v["begin"]:v["end"]]
+                        for j, secquence in enumerate(entity.split (" ")):
+                            if (j == 0):
+                                words.append(secquence)
+                                tags.append(f"B-{v['type']}")
+                            else:
+                                words.append(secquence)
+                                tags.append(f"I-{v['type']}")
+                    
+                        i = v["end"]
+                if not entity_found:
+                    if (i < o_tags_idx[current_o_tags_idx][0]):
+                        i += 1
+                        continue
+                    word = text[o_tags_idx[current_o_tags_idx][0]:o_tags_idx[current_o_tags_idx][1]]
+
+                    words.append(word)
+                    tags.append("O")
+                    if current_o_tags_idx == len(o_tags_idx)-1:
+                        break
+                    else:
+                        i += 1
+                        current_o_tags_idx+=1
+            break
+
+
+    df = pd.DataFrame({"Words": words, "Label": tags})
+    return df
+
 if __name__ == '__main__':
     dict = construct_docMap("data/re3d-master/US State Department/entities.json")
-    df = map_entities(dict, "data/re3d-master/US State Department/documents.json")
+    df = map_entities_pickFirstMatch(dict, "data/re3d-master/US State Department/documents.json")
     print(df.to_string())
 
 ## ATJ test
