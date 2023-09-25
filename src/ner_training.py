6a6f42a052f3a663829a380bb0d819f470cc3678
Lasse Askholm <laask@dc1-proj287.systematicgroup.local>
2023-09-25 12:51:43 +0000

    Changed ner_learning to work with requirements

--- src/ner_training.py
+++ src/ner_training.py
@@ -4,7 +4,7 @@ import numpy as np
 from datasets import Dataset
 from datasets import load_metric
 from transformers import AutoTokenizer
-from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer, AutoModelForCausalLM
+from transformers import AutoModelForTokenClassification, TrainingArguments, Trainer, AutoModelForCausalLM, GPTQConfig
 from transformers import DataCollatorForTokenClassification
 from LoadTestData import construct_global_docMap, map_all_entities
 
@@ -16,7 +16,7 @@ def load_labels(labels_path):
     
     mapped_labels = {}
     
-    for i, labels in enumerate(labels):
+    for i, label in enumerate(labels):
         mapped_labels[label] = i
     
     return mapped_labels, labels
@@ -30,29 +30,26 @@ def load_data_sets():
     return (train_data, test_data)
     
 def tokenize_labels(examples, tokenizer, mapped_labels):
-    tokenized_inputs = tokenizer(list(examples["Words"]),truncation = true, is_split_into_words = true)
-    label_all_tokens = False
+    tokenized_inputs = tokenizer(list(examples["Words"]), truncation = True)
     labels = []
-    
-    for i , label in enumerate (examples["Label"]):
-        word_ids  = tokenized_inputs.word_ids(batch_index = i)
+
+    for i, label in enumerate (examples["Label"]):
+        word_ids = tokenized_inputs.word_ids(batch_index = i)
         previous_word_idx = None
         label_ids = []
         for word_idx in word_ids:
             if word_idx is None:
                 label_ids.append(-100)
-            elif label [word_idx] == '0':
-                label_ids.append(0)
             elif word_idx != previous_word_idx:
-                label_ids.append(mapped_labels[label[word_idx]])
+                label_ids.append(mapped_labels[label])
             else:
-                label_ids.append(mapped_labels[label[word_idx]] if label_all_tokens else -100)
+                label_ids.append(-100)
             previous_word_idx = word_idx
-            
         labels.append(label_ids)
-        tokenized_inputs["Label"] = labels
-        
-        return tokenized_inputs 
+
+    print(labels)
+    tokenized_inputs["labels"] = labels
+    return tokenized_inputs 
 
 def compute_metrics(p):
     '''
@@ -74,30 +71,29 @@ def main ():
     
     #Loading and tokenizing datasets
     train_data, test_data = load_data_sets()
-    labels, mapped_labels = load_labels("../resources/labels.txt")
-    
-    train_tokenized_dataset = train_data.map(tokenize_labels, batched=True, fn_kwargs={"tokenizer": tokenizer, "label_encoding_dict": mapped_labels})
-    
-    test_tokenized_dataset = test_data.map(tokenize_labels, batched=True, fn_kwargs={"tokenizer": tokenizer, "label_encoding_dict": mapped_labels})
-  
+    mapped_labels, labels = load_labels("../resources/labels.txt")
     
     #tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf")
+    tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-hf", token=access_token)
+
+    train_tokenized_dataset = train_data.map(tokenize_labels, batched=True, fn_kwargs={"tokenizer": tokenizer, "mapped_labels": mapped_labels})
+    
+    test_tokenized_dataset = test_data.map(tokenize_labels, batched=True, fn_kwargs={"tokenizer": tokenizer, "mapped_labels": mapped_labels})
     
     #data collator
     data_collator = DataCollatorForTokenClassification(tokenizer)
     
     #Configure quantization
-    gptq_config = GPTQConfif(bits = 4 , dataset = train_tokenized_dataset, tokenizer = tokenizer)
+    #gptq_config = GPTQConfig(bits = 4 , dataset = train_tokenized_dataset, tokenizer = tokenizer)
     
     #model
-    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf",num_labels = len(labels), quantization = gptq_config)
+    model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-hf",num_labels = len(labels), token=access_token)
     
     ### define training args here
     ### we should experiment with these hyperparameters.
     ### Leaning rates from bert documentation = (among 5e-5, 4e-5, 3e-5, and 2e-5)
     training_args = TrainingArguments(
-        os.path.join(get_root_path(), 'models',"/outputDir"),
+        output_dir="../models",
         evaluation_strategy = "epoch",
         save_strategy='epoch',
         learning_rate=2e-5,
@@ -105,6 +101,8 @@ def main ():
         per_device_eval_batch_size=16,
         num_train_epochs=30,
         weight_decay=1e-5,
+        load_best_model_at_end=True,
+        push_to_hub=True
     )
     
     ### define trainer here
@@ -123,7 +121,7 @@ def main ():
 
     #start training model
     
-    #trainer.train()
+    trainer.train()
     
 
 if __name__ == '__main__':
