710202f1a20b7bd88e1b17f6a23358efd6fd4410
Desktop-ATJ <akselthyregod@live.dk>
2023-10-03 13:16:01 +0200

    gui start

---
+++ gui/results_table.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+# https://www.geeksforgeeks.org/create-table-using-tkinter/
+ 
+class Table:
+     
+    def __init__(self, root, json):
+         
+        results = self.json_to_list(json)
+
+        total_rows, total_columns = len(results), len(results[0]) - 2
+        # code for creating table
+        # Column "Headers"
+        for i in range(total_columns):
+            self.e = Entry(root, width=15, fg='black',
+                               font=('Arial',16,'bold'))
+            self.e.grid(row=0, column=i)
+            self.e.insert(END, results[0][i][0])
+
+        # Results
+        for i in range(total_rows):
+            for j in range(total_columns):
+                 
+                self.e = Entry(root, width=15, fg='blue',
+                               font=('Arial',16,'bold'))
+                 
+                self.e.grid(row=i+1, column=j)
+                self.e.insert(END, results[i][j][1])
+
+    def json_to_list(self, json):
+        list = []
+        for element in json:
+            list.append(tuple(element.items()))
+        return list
