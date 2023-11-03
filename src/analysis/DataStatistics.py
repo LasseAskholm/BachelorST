f512df12ce234148155bfa3a93667153219ede4d
LasseAskholm <71758394+LasseAskholm@users.noreply.github.com>
2023-11-03 13:45:21 +0100

    data stats

--- src/analysis/DataStatistics.py
+++ src/analysis/DataStatistics.py
@@ -1,45 +1,73 @@
-from AmbigiousTags import *
+import itertools
+import json
+import pandas as pd
 
+def create_correct_labels_map():
+    tag_map = {}
 
-def create_stats_map(path):
-     tag_map = {}
-     
-     with open(path,'r',encoding="utf-8") as f:
+    data_path ="../../data/selv-labeled-data/fixed_v5/v5.conll"
+    with open(data_path,'r',encoding="utf-8") as f:
         lines = f.readlines()
         split_list = [list(y) for x, y in itertools.groupby(lines, lambda z: z == '\n') if not x]
         for y in split_list:
             for x in y:
                 text , label = x.split('\t')
                 label = label.strip('\n')
-                
-     
+                if text not in tag_map.keys():
+                    tag_map[text]= label
+                continue
+    return tag_map
 
-
-
-
-
-
-
+def create_stats_map(path):
+    tag_map = {}
+    
+    with open(path,'r',encoding="utf-8") as f:
+      lines = f.readlines()
+      split_list = [list(y) for x, y in itertools.groupby(lines, lambda z: z == '\n') if not x]
+      for y in split_list:
+          for x in y:
+              text , label = x.split('\t')
+              label = label.strip('\n')
+              if text not in tag_map.keys():
+                tag_map[text] = []
+                tag_map[text].append(label)
+                continue
+              tag_map[text].append(label)
+    return tag_map                  
+                    
 def stats ():
   correctLabels = create_correct_labels_map()
-
-
-  json_path ="../../data/selv-labeled-data/fixed_v0/selfLabeledDataJSONFiltered.json"
-  conll_path = "../../data/selv-labeled-data/fixed_v0/selfLabeledDataFiltered.conll"
+  conll_path = "../../data/selv-labeled-data/fixed_v5/v5.conll"
   actualLabelsV0 = create_stats_map(conll_path)
-  calculatePureness(data_map=actualLabelsV0, correct_map=correctLabels)
+  scores = calculatePureness(data_map=actualLabelsV0, correct_map=correctLabels)
+  df_scores = pd.DataFrame.from_dict(scores, orient='index')
+  df_scores.to_csv('correctnessv5.csv', encoding='utf-8')
+  
 def calculatePureness(data_map, correct_map):
-   scores = {}
-   print(len(data_map.keys()))
-
-   for key in data_map.keys():
-    print(key)
-    print("\n")
+  scores = {}
+  for word in data_map.keys():
+    correctCount = 0
+    incorrectCount = 0
+    if word == "-DOCSTART-":
+      continue
+    correct_label = correct_map[word]
+    for label in data_map[word]:
+      if label == correct_label:
+        correctCount += 1
+        continue
+      incorrectCount += 1 
+    total_num_labels = len(data_map[word])
+    correctPercent = (correctCount/total_num_labels)*100
+    incorrectPercent = (incorrectCount/total_num_labels)*100
+    scores[word] = {}
+    scores[word]['Correct'] = correctPercent
+    scores[word]['Incorrect'] = incorrectPercent
+    scores[word]['Count'] = total_num_labels
+    
+  return scores
 
 def main():
   stats()
-
-
 
 if __name__ == '__main__':
     main()
